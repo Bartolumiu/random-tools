7f816c64b1567b7247aba5e53ac1a95ee4543585
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-08-16 18:16:57 +0200

    Update mass_divider.py
    Added support for .jpeg, .bmp, .gif, .tiff, .webp and .jfif

--- images/divider/mass_divider.py
+++ images/divider/mass_divider.py
@@ -1,54 +1,54 @@
 import argparse
 import os
+import pillow_jxl
 from PIL import Image
 
 def divide_image_vertically(input_image_path, output_directory, num_parts):
-    # Open the input image
-    input_image = Image.open(input_image_path)
-    
-    # Get dimensions of the original image
-    width, height = input_image.size
-    
-    # Calculate height of each part
-    part_height = height // num_parts
-    
-    # Iterate through each part
-    for i in range(num_parts):
-        # Calculate coordinates for cropping each part
-        left = 0
-        upper = i * part_height
-        right = width
-        lower = (i + 1) * part_height
-        
-        # Crop the image to extract the part
-        cropped_image = input_image.crop((left, upper, right, lower))
 
-        # Create a new subdirectory for each input image with the same name
-        part_output_directory = os.path.join(output_directory, os.path.splitext(os.path.basename(input_image_path))[0])
-        os.makedirs(part_output_directory, exist_ok=True)
-        
-        # Generate output filename with sequential numbering like '001.png', '002.png', ..., '999.png'
-        output_filename = f"{i+1:03d}.png"
-        output_path = os.path.join(part_output_directory, output_filename)
-        
-        # Save each part with a unique name
-        cropped_image.save(output_path)
-        print(f"Saved part {i+1} to {output_path}")
+    with Image.open(input_image_path) as input_image:
+        # Get dimensions of the original image
+        width, height = input_image.size
     
-    print(f"Image divided into {num_parts} parts.")
+        # Calculate height of each part
+        part_height = height // num_parts
+    
+        # Iterate through each part
+        for i in range(num_parts):
+            # Calculate coordinates for cropping each part
+            left = 0
+            upper = i * part_height
+            right = width
+            lower = (i + 1) * part_height
+        
+            # Crop the image to extract the part
+            cropped_image = input_image.crop((left, upper, right, lower))
+
+            # Create a new subdirectory for each input image with the same name
+            part_output_directory = os.path.join(output_directory, os.path.splitext(os.path.basename(input_image_path))[0])
+            os.makedirs(part_output_directory, exist_ok=True)
+        
+            # Generate output filename with sequential numbering like '001.png', '002.png', ..., '999.png'
+            output_filename = f"{i+1:03d}.png"
+            output_path = os.path.join(part_output_directory, output_filename)
+        
+            # Save each part with a unique name
+            cropped_image.save(output_path)
+            print(f"Saved part {i+1} to {output_path}")
+    
+        print(f"Image divided into {num_parts} parts.")
 
 def run(input_dir, output_dir, num_parts):
     # Iterate through each file in the input directory
     for filename in os.listdir(input_dir):
         # Check if the file is an image
-        if filename.endswith(".png") or filename.endswith(".jpg"):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.webp', '.jfif')):
             # Generate the full path to the input image
             input_image_path = os.path.join(input_dir, filename)
             
             # Call the divide_image_vertically function
             divide_image_vertically(input_image_path, output_dir, num_parts)
         else:
-            print(f"Skipping non-image file: {filename}")
+            print(f"Skipping non-image or non-supported file: {filename}")
 
 if __name__ == "__main__":
     # Setup argument parser
